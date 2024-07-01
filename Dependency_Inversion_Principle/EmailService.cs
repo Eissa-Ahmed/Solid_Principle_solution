0f956a049130c250a22b509bed9970a8493635e6
Essa Ahmed <essa1998ahmed@gmail.com>
2024-07-01 08:55:56 +0300

    00

---
+++ Dependency_Inversion_Principle/EmailService.cs
@@ -0,0 +1,34 @@
+﻿namespace Dependency_Inversion_Principle;
+public interface IMessage
+{
+    public void Send(string message);
+}
+
+public class EmailService : IMessage
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Email {message}");
+    }
+}
+public class SmsService : IMessage
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Sms {message}");
+    }
+}
+
+public class Noti
+{
+    private readonly IMessage _message;
+    public Noti(IMessage message)
+    {
+        _message = message;
+    }
+
+    public void Notify(string message)
+    {
+        _message.Send(message);
+    }
+}
--- Dependency_Inversion_Principle/Program.cs
+++ Dependency_Inversion_Principle/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Noti noti = new Noti(new SmsService());
+            noti.Notify("Hello");
         }
     }
 }
